dd9b23846b93d96b9af4d8638cae13f187f7505b
Nathan Foreman <Nathan@Practice2Perfect.com>
2020-12-16 20:09:05 +0000

    Updated to use a local path of OfflineTuningStore

--- ConvertTo-CustomStoreFormat.ps1
+++ ConvertTo-CustomStoreFormat.ps1
@@ -1,11 +1,11 @@
-Remove-Item -Path .\CustomStoreFormat -Force -ErrorAction SilentlyContinue 
-New-Item -Type Directory -Name 'CustomStoreFormat'
+Remove-Item -Path .\OfflineTuningStore -Force -ErrorAction SilentlyContinue 
+New-Item -Type Directory -Name 'OfflineTuningStore'
 $index = Get-Content .\Index.json | ConvertFrom-Json
 foreach($tp in $index){
 $sourceDetailsFile = ".\$($tp.TuningPackName)\details.csv"
 $sourceMetaDataFile = ".\$($tp.TuningPackName)\metadata.json"
-$destinationDetailsFile = ".\CustomStoreFormat\$($tp.TuningPackName).csv"
-$destinationMetaDataFile = ".\CustomStoreFormat\$($tp.TuningPackName).metadata.json"
+$destinationDetailsFile = ".\OfflineTuningStore\$($tp.TuningPackName).csv"
+$destinationMetaDataFile = ".\OfflineTuningStore\$($tp.TuningPackName).metadata.json"
 
 Copy-Item $sourceDetailsFile $destinationDetailsFile
 Copy-Item $sourceMetaDataFile $destinationMetaDataFile
